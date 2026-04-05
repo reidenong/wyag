4f78c3089464ca569a8c7283e5f86ad5a2b9f2f8
reidenong <reidenong02@gmail.com>
2026-04-06 01:35:57 +0800

    implement resolve ref

---
+++ include/wyag/ref.hpp
@@ -0,0 +1,24 @@
+#pragma once 
+
+#include <optional>
+#include <string>
+#include <filesystem>
+#include <vector>
+#include "git_repository.hpp"
+
+struct DirectRef {
+    std::string name {};
+    std::string sha {};
+};
+
+struct RefDirectory {
+    std::string name{};
+    std::vector<RefDirectory> subdir{};
+    std::vector<DirectRef> refs{};
+};
+
+// Recursively resolves references to a sha identifier if it exists
+std::optional<std::string> resolve_ref(const GitRepository& repo, std::filesystem::path path);
+
+// Lists all references in a directory
+RefDirectory list_repo_refs(const GitRepository& repo, std::filesystem::path path);
